f79d0f6a2e932ad580b47f7945e7048225313a42
salabh-an <salabh.n@aot-technologies.com>
2022-03-29 10:00:57 +0530

    - sync form data api   - handle nested relations - added models/tables for inspection - added column `ea_certificate` to projects

--- src/reports_api/services/sync_form_data.py
+++ src/reports_api/services/sync_form_data.py
@@ -15,6 +15,7 @@
 
 from flask import current_app
 from inflector import English, Inflector
+from typing import Union
 
 from reports_api.utils.helpers import find_model_from_table_name
 
@@ -24,7 +25,8 @@ class SyncFormDataService:  # pylint:disable=too-few-public-methods
 
     @classmethod
     def _update_or_create(cls, model_class, data: dict):
-        current_app.logger.debug(f"_update_or_create with model {model_class} and data {data}")
+        """Create or update model instance from data and model class"""
+        current_app.logger.debug(f"_update_or_create with model {model_class}")
         # Get the list of column names for the model
         mapper = model_class.__mapper__
         columns = dict(mapper.columns).keys()
@@ -40,21 +42,66 @@ class SyncFormDataService:  # pylint:disable=too-few-public-methods
         return obj
 
     @classmethod
-    def _process_model_data(cls, model_name: str, dataset: dict):
-        result = None
+    def _get_model_name_and_relations(cls, model_key: str, data: dict, result: dict):
+        """Get the model's name and foreign key relations from data dict"""
+        model_name = model_key
+        relations_list = []
+        current_app.logger.info(f'Get model name and relations for {model_key}')
+        if '-' in model_key:
+            *relations_list, model_name = model_key.split('-')
+            for relation in relations_list:
+                if relation not in result:
+                    payload_relation_key = next((x for x in data.keys() if x == relation),
+                                                next((x for x in data.keys() if x.endswith(f'-{relation}')), None))
+                    current_app.logger.info(f'payload_relation_key is {payload_relation_key}')
+                    result[relation] = cls._process_model_data(relation, data[payload_relation_key], result)
+        return model_name, relations_list, result
+
+    @classmethod
+    def _process_model_instance_data(cls, model_class, data: dict, result: dict):
+        """Process data for a single instance of a model"""
+        inflector = Inflector(English)
+        dependants = [{k: v} for k, v in data.items() if isinstance(v, dict) or isinstance(v, list)]
+        obj = cls._update_or_create(model_class, data)
+        current_app.logger.info(f'Model class ---> {model_class}')
+        instance = obj.as_dict()
+        if instance:
+            for dependant in dependants:
+                current_app.logger.info(f'Processing dependants')
+                foreign_keys = {}
+                key, value = next(iter(dependant.items()))
+                if not hasattr(model_class, key):
+                    model_key = key.replace(f'{model_class.__tablename__}-', '')
+                    model_name, relations, result = cls._get_model_name_and_relations(model_key, data, result)
+                    if find_model_from_table_name(model_name) is not None:
+                        for relation in relations:
+                            relation_key = inflector.singularize(relation)
+                            foreign_keys[f'{relation_key}_id'] = result[relation]['id']
+                        instance_name = inflector.singularize(model_class.__tablename__)
+                        foreign_keys[f'{instance_name}_id'] = instance['id']
+                        current_app.logger.info(f'foreign_keys {foreign_keys}')
+                        if isinstance(value, dict):
+                            dependant.update(foreign_keys)
+                        elif isinstance(value, list):
+                            dependant = [{**x, **foreign_keys} for x in value]
+                    instance[key] = cls._process_model_data(model_name, dependant, result)
+        return instance
+
+    @classmethod
+    def _process_model_data(cls, model_name: str, dataset: Union[dict, list], result: dict):
+        """Process dataset for a model"""
+        instance = None
         model_class = find_model_from_table_name(model_name)
 
         if model_class:
-            # TODO: Look into bulk insert/update for collection of items in payload
             if isinstance(dataset, dict):
-                obj = cls._update_or_create(model_class, dataset)
-                result = obj.as_dict()
+                instance = cls._process_model_instance_data(model_class, dataset, result)
             elif isinstance(dataset, list):
-                result = []
+                instance = []
                 for data in dataset:
-                    obj = cls._update_or_create(model_class, data)
-                    result.append(obj.as_dict())
-        return result
+                    obj = cls._process_model_instance_data(model_class, data, result)
+                    instance.append(obj)
+        return instance
 
     @classmethod
     def sync_data(cls, payload: dict):
@@ -65,23 +112,16 @@ class SyncFormDataService:  # pylint:disable=too-few-public-methods
         for model_key, dataset in payload.items():  # pylint:disable=too-many-nested-blocks
             if model_key not in result:
                 foreign_keys = {}
-                model_name = model_key
-                if '-' in model_key:
-                    *relations_list, model_name = model_key.split('-')
-                    for relation in relations_list:
-                        if relation not in result:
-                            payload_relation_key = next((x for x in payload.keys() if x == relation), None)
-                            if payload_relation_key is None:
-                                payload_relation_key = next(
-                                    (x for x in payload.keys() if x.endswith(f'-{relation}')), None)
-                            result[relation] = cls._process_model_data(relation, payload[payload_relation_key])
-                        relation_key = inflector.singularize(relation)
-                        foreign_keys[f'{relation_key}_id'] = result[relation]['id']
+                model_name, relations, result = cls._get_model_name_and_relations(model_key, payload, result)
+                for relation in relations:
+                    relation_key = inflector.singularize(relation)
+                    foreign_keys[f'{relation_key}_id'] = result[relation]['id']
             if isinstance(dataset, dict):
                 dataset.update(foreign_keys)
             elif isinstance(dataset, list):
                 dataset = [{**x, **foreign_keys} for x in dataset]
-            obj = cls._process_model_data(model_name, dataset)
+            current_app.logger.info(f'Processing model data for {model_name}')
+            obj = cls._process_model_data(model_name, dataset, result)
             if obj:
                 result[model_key] = obj
         return result
